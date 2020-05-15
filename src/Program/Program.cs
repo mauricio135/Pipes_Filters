54b098a2fbed3407a4270115b81685e792d32daf
Levi de los Santos <levidlsantos@gmail.com>
2020-05-15 20:39:49 -0300

    Ejercicio 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -10,12 +10,16 @@ namespace CompAndDel
         {
             IFilter Greyscale = new FilterGreyscale();
             IFilter Negative = new FilterNegative();
+            IFilter Save = new FilterSave();
+
             IPipe Pipe3 = new PipeNull();
-            IPipe Pipe2 = new PipeSerial(Negative, Pipe3);
-            IPipe Pipe1 = new PipeSerial (Greyscale, Pipe2);
+            IPipe PipeSave2 = new PipeSerial(Save,Pipe3);
+            IPipe Pipe2 = new PipeSerial(Negative, PipeSave2);            
+            IPipe PipeSave1 = new PipeSerial(Save,Pipe2);
+            IPipe Pipe1 = new PipeSerial (Greyscale, PipeSave1);
             
             PictureProvider p = new PictureProvider();
-            IPicture pic = p.GetPicture("..\\picture.jpg");
+            IPicture pic = p.GetPicture("..\\Images\\picture.jpg");
 
             Pipe1.Send(pic);
         }
